8a32c9b20ffa8efc526a50cdeba2d044096a2eb8
Aaron Mills <homer2029@gmail.com>
2024-03-19 21:26:41 -0500

    update comments

--- chapter_9/exercise_9_1/exercise_9_1.py
+++ chapter_9/exercise_9_1/exercise_9_1.py
@@ -3,6 +3,7 @@
 # init takes two arguments cusine type and name
 # methods describe resturant and open resturant
 # instantiate the resturant
+# separate file and import not required
 from resturant import Resturant
 
 resturant_1 = Resturant("aaron's place", 'bbq')
