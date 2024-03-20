a4a1a484364c47db0cef975c2af3785141aec3ee
Aaron Mills <homer2029@gmail.com>
2024-03-19 21:25:04 -0500

    add exercise 9-1 and resturant

---
+++ chapter_9/exercise_9_1/exercise_9_1.py
@@ -0,0 +1,11 @@
+# exercise 9-1 resturant
+# resturant class
+# init takes two arguments cusine type and name
+# methods describe resturant and open resturant
+# instantiate the resturant
+from resturant import Resturant
+
+resturant_1 = Resturant("aaron's place", 'bbq')
+
+resturant_1.describe_resturant()
+resturant_1.open_resturant()
