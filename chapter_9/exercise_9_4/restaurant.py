2367aaa20b6ee71e40ea6950846fd9deb5b2b8f7
Aaron Mills <homer2029@gmail.com>
2024-03-24 11:06:28 -0500

    updates to restaurant; more examples in exercise 9-4

--- chapter_9/exercise_9_4/restaurant.py
+++ chapter_9/exercise_9_4/restaurant.py
@@ -18,9 +18,39 @@ class Restaurant:
         """
         print(f' * name: {self.name.title()}')
         print(f' * cusine_type: {self.cusine_type.title()}')
-        print(f' * number_served: {self.number_served}\n')
+        print(f' * number_served: {self.number_served:,}\n')
 
     def open_restaurant(self) -> None:
         """ Prints a message indicating the restaurant is open.
         """
         print(f'{self.name.title()} is now open.')
+
+    def set_number_served(self, number_served: int) -> None:
+        """ Sets the value for number served.
+
+        Args:
+            number_served (int): The number of persons served. 
+            Must be positive and greater than the current number served.
+        """
+        if 0 > number_served:
+            print('number_served must be positive')
+            return
+
+        if self.number_served >= number_served:
+            print(f'number_served({number_served}) must be greater than the current number_served({
+                  self.number_served})')
+            return
+
+        self.number_served = number_served
+
+    def increment_number_served(self, number_served: int) -> None:
+        """ Adds a value to the number served.
+
+        Args:
+            number_served (int): The number to increment by.
+        """
+        if 0 >= number_served:
+            print('number_served must be positive and greater than zero.')
+            return
+
+        self.number_served += number_served
